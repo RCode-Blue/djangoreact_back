a7e901b103ebbd888c06cf713fcb7d74d248250a
Ricky <rickyliew@hotmail.com>
2020-02-25 13:01:59 +1100

    Heroku deploy #44

--- src_backend/config/prod_settings.py
+++ src_backend/config/prod_settings.py
@@ -1,5 +1,7 @@
 import os
 
+debug = True
+
 # SECRET_KEY = '5j_!%*1@aqyhn%%zff#l6n%4lecm_5*)jrsfd4$fmvzf=-)25u'
 SECRET_KEY = os.urandom(24)
 ALLOWED_HOSTS = ["vast-sierra-07062.herokuapp.com", "0.0.0.0", "localhost"]
